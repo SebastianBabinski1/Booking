ef16d7abb47c8327177fe0208c487262cb381689
SebastianBabinski1 <sebastian.babinski1@gmail.com>
2022-10-04 21:17:28 +0200

    Implementation of basic operations on backend (deleting, updating rooms, hotels...)

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,28 +5,32 @@ import {
   getUser,
   getAllUsers,
 } from "../controllers/user.js";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-  res.send("hello user, you are logged in");
-});
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("hello user, you are logged in");
+// });
 
-router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-  res.send("hello user, you are logged in and you can delete your account");
-});
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("hello user, you are logged in and you can delete your account");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("hello admin, you are logged in and you can delete all accounts");
+// });
 
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 // GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 // GET ALL
-router.get("/", getAllUsers);
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;
